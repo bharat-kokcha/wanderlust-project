39277af9d070306317be83e4aefa56753f8a1f5b
ApshCode <Apshcode@Gmail.com>
2025-04-18 06:33:34 +0530

    Add user controller for auth routes

---
+++ controllers/user.js
@@ -0,0 +1,10 @@
+// controllers/user.js
+const User = require('../models/user');
+// signup, login, logout controller functions...
+module.exports = {
+  renderRegister,
+  registerUser,
+  renderLogin,
+  loginUser,
+  logoutUser
+};
